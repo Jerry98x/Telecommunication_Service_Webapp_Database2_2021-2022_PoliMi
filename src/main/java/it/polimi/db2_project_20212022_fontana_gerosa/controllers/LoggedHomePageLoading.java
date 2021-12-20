9276590b24631777cacd7812895f2334b4a71148
Nicolò Fontana <nicolo.fontana@mail.polimi.it>
2021-12-20 12:24:47 +0100

    Add different homepage loading classes

---
+++ src/main/java/it/polimi/db2_project_20212022_fontana_gerosa/controllers/LoggedHomePageLoading.java
@@ -0,0 +1,75 @@
+package it.polimi.db2_project_20212022_fontana_gerosa.controllers;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import it.polimi.db2_project_20212022_fontana_gerosa.HomePageContent;
+import it.polimi.db2_project_20212022_fontana_gerosa.beans.Order;
+import it.polimi.db2_project_20212022_fontana_gerosa.beans.ServicePackage;
+import it.polimi.db2_project_20212022_fontana_gerosa.beans.User;
+import it.polimi.db2_project_20212022_fontana_gerosa.services.OrderService;
+import it.polimi.db2_project_20212022_fontana_gerosa.services.ServicePackageService;
+import jakarta.persistence.PersistenceException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/LoggedHomePageLoading")
+public class LoggedHomePageLoading extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        OrderService orderService = new OrderService();
+        List<Order> rejectedOrders = new ArrayList<>();
+        List<Order> pendingOrders = new ArrayList<>();
+        ServicePackageService servicePackageService = new ServicePackageService();
+        List<ServicePackage> servicePackages = new ArrayList<>();
+        if(user.isInsolvent()) {
+            try {
+                rejectedOrders = orderService.getRejectedOrders(user);
+            } catch (PersistenceException e) {
+                e.printStackTrace();
+                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                response.getWriter().println("Not possible to recover rejected orders");
+                return;
+            }
+        }
+
+        try {
+            pendingOrders = orderService.getPendingOrders(user);
+        } catch (PersistenceException e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().println("Not possible to recover pending orders");
+            return;
+        }
+
+        try {
+            servicePackages = servicePackageService.getAllServicePackages();
+        } catch (PersistenceException e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().println("Not possible to recover service packages");
+            return;
+        }
+
+        // Redirect to the Home page and add servicePackages to the parameters
+
+        Gson gson = new GsonBuilder().create();
+
+        HomePageContent hpc = new HomePageContent(user.getUsername(), user.isInsolvent(), rejectedOrders, pendingOrders, servicePackages);
+        String json = gson.toJson(hpc);
+
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
+    }
+}
--- src/main/java/it/polimi/db2_project_20212022_fontana_gerosa/services/OrderService.java
+++ src/main/java/it/polimi/db2_project_20212022_fontana_gerosa/services/OrderService.java
@@ -1,7 +1,22 @@
 package it.polimi.db2_project_20212022_fontana_gerosa.services;
 
+import it.polimi.db2_project_20212022_fontana_gerosa.beans.Order;
+import it.polimi.db2_project_20212022_fontana_gerosa.beans.ServicePackage;
+import it.polimi.db2_project_20212022_fontana_gerosa.beans.User;
 import jakarta.persistence.EntityManager;
+
+import java.util.List;
 
 public class OrderService {
     private EntityManager em;
+
+    public List<Order> getRejectedOrders(User user){
+        return em.createNamedQuery("Order.getRejectedOrders", Order.class)
+                .setParameter(1, user.getUserId()).getResultList();
+    }
+
+    public List<Order> getPendingOrders(User user){
+        return em.createNamedQuery("Order.getPendingOrders", Order.class)
+                .setParameter(1, user.getUserId()).getResultList();
+    }
 }
