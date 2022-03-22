0817c8c6cce633d4f41797492dc5c2c1bcbe1fa8
Nicolò Fontana <nicolo.fontana@mail.polimi.it>
2022-03-22 10:51:45 +0100

    Fix composite key methods

--- src/main/java/it/polimi/db2_project_20212022_fontana_gerosa/beans/mv/SpVpKey.java
+++ src/main/java/it/polimi/db2_project_20212022_fontana_gerosa/beans/mv/SpVpKey.java
@@ -1,6 +1,7 @@
 package it.polimi.db2_project_20212022_fontana_gerosa.beans.mv;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class SpVpKey implements Serializable {
     private int servicePackageId;
@@ -15,5 +16,18 @@ public class SpVpKey implements Serializable {
 
     public int getValidityPeriodId() {
         return validityPeriodId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SpVpKey spVpKey = (SpVpKey) o;
+        return servicePackageId == spVpKey.servicePackageId && validityPeriodId == spVpKey.validityPeriodId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(servicePackageId, validityPeriodId);
     }
 }
